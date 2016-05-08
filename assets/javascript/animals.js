aa3128046fafcab4e11213ae3cf7c6f9bfa2afc2
John D. Riepe <JohnD.Riepe@danielles-mbp-2.attlocal.net>
2016-05-07 23:18:48 -0400

    initial commit...added buttons

---
+++ assets/javascript/animals.js
@@ -0,0 +1,71 @@
+// Javascript Giphy Game
+///////////////////////////	
+
+
+    function startProgram(){
+        var i=0;
+        var animals = ["dog", "cat", "horse", "whale", "dolphin", "fox", "wolf", "tiger", "lion"];
+
+        for (i=0;i<animals.length;i++)  {
+            console.log (animals[i]);
+            console.log("-------------------");
+            $('#theButtons').append('<button id="aniButtons">'+animals[i]+'</button>')
+        }  // end for loop
+}  //end function startProgram()
+
+      
+
+
+
+
+                  
+    // on click functions
+
+                        
+                       // $('#ans2').on('click', function() {
+                            
+                            
+                        // }) // end on click
+                
+                            
+                        
+                           
+
+
+                       
+                            
+                        
+// end on click functions
+                  
+          
+
+
+
+
+
+
+       //---------------FILL IN CODE HERE FOR STEP TWO----------------------------
+         //   var state = $(this).attr("data-state");
+         //   console.log(state);
+          //----------------------------------------------------
+
+          
+
+          //---------------FILL IN CODE HERE FOR STEP THREE----------------------------
+          //  if (state == 'still') {
+           //     $(this).attr("src",$(this).attr("data-animate"));
+           //      $(this).attr("data-state","animate"); // n
+         //   }
+
+          //  else  {
+        //        $(this).attr("src",$(this).attr("data-still"));
+         //       $(this).attr("data-state","still");
+        //    }
+            //----------------------------------------------------
+
+
+            
+      
+
+
+              
